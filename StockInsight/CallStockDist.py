0d0b83afc7ab3eebc5a95c837c39173ddf709c75
Samuel Rod-Wal <83095093+SRodWal@users.noreply.github.com>
2021-06-22 14:02:09 -0500

    Update CallStockDist.py

--- StockInsight/CallStockDist.py
+++ StockInsight/CallStockDist.py
@@ -3,9 +3,10 @@ import numpy as np
 import yfinance as yf
 import pandas_datareader as pdr
 import datetime as dt
-import matplotlib.pyplot as plt
+import seaborn as sb
 
 # A script to analyze stock prices and temproral changes.
+# This is designed for medium-short term investments (No interdaily informacion)
 
 
 #Define input variables
@@ -14,6 +15,14 @@ TimeWindow = [dt.datetime(2019,1,1),dt.datetime.now()]
 
 #Collect data
 df = pdr.get_data_yahoo(StockList[0],TimeWindow[0],TimeWindow[1])
-print(df)
+
+#Initial Stock description:
+    #Valume movement graphs
+sb.lineplot(data = df, x = "Date", y = "Volume")
+    #Prices daily range
+    #Initial & closeing values
+    
+
+
 
 

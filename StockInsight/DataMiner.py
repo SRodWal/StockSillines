de1eddf886e5939081d751f68258859ddb8cc70a
Samuel Rod-Wal <83095093+SRodWal@users.noreply.github.com>
2021-06-26 21:59:01 -0500

    Update DataMiner.py

--- StockInsight/DataMiner.py
+++ StockInsight/DataMiner.py
@@ -1,31 +1,29 @@
 #Yahoo Finance & Data collection
 import yfinance as yf
 import pandas as pd
+import os
 #Numeric and data treatment
 import numpy as np
 import datetime as dt
 import seaborn as sb
 #Graphs
 import matplotlib.pyplot as plt
-import plotly.graph_pbjects as go
+import plotly.graph_objects as go
 
 # A script to analyze stock prices and temproral changes.
 # This is designed for medium-short term investments (No interdaily informacion)
 
 
-#Define input variables
+###Define input variables
 StockList = ["BA","FSLR","HQCL"] #List of Stocks
 NameList = ["Boeing Co.", "First Solar, Inc." "Hanwha Q CELLS Co."] # Names of the tickers
 period = "5wk" #Time backwards
-interval = "5m" # Time between mesurements 
+interval = "5m" # Time between mesurements  
 
-#Initial Stock description:
-    #Valume movement graphs
-sb.lineplot(data = df, x = "Date", y = "Volume")
-plt.title("Volume - "+StockList[0])
-    #Prices daily range
-    #Initial & closeing values
-    
+
+###Objective folder
+filedir = os.getcwd()+"\\Data"   
+# Read files in data folder 
 
 
 

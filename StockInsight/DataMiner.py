02c92e207c2a9d614daba7337429fa9e1f3f466a
Samuel Rod-Wal <83095093+SRodWal@users.noreply.github.com>
2021-06-28 17:48:32 -0500

    update

--- StockInsight/DataMiner.py
+++ StockInsight/DataMiner.py
@@ -18,8 +18,8 @@ import plotly.express as px
 
 
 ###Define input variables
-StockList = ["BA","FSLR","HQCL"] #List of Stocks
-NameList = ["Boeing Co", "First Solar, Inc", "Hanwha Q CELLS Co"] # Names of the tickers
+StockList = ["BA","FSLR","PCRFY"] #List of Stocks
+NameList = ["Boeing Co", "First Solar, Inc", "Panasonic Corp"] # Names of the tickers
 period = "5wk" #Time backwards
 interval = "5m" # Time between mesurements  
 filedir = os.getcwd()
@@ -36,12 +36,15 @@ datadir = filedir+"\\DATA"
 for ticker,name in zip(StockList, NameList):
     Rawdata = yf.Ticker(ticker) #Retrieves all stock information
     data = Rawdata.history(period = "max") # Stock daily historic data
+    textdate = [time.strftime("%m/%d/%Y") for time in data.index]
+    Histperiod = textdate[0]+" - "+textdate[-1]
     #data = data.reset_index()
     #candledata = go.Candlestick(x = data["Datetime"] ,open = data["Open"], high = data["High"],low = data["Low"], close = data["Close"])
-    candledata = go.Candlestick(open = data.Open, high = data.High,low = data.Low, close = data.Close)
+    candledata = go.Candlestick(x = data.index.tolist(), open = data.Open, high = data.High,low = data.Low, close = data.Close)
     fig = go.Figure(data = [candledata])
-    fig.update_layout(title = "Historic "+"name"+" stock prices", )
-    #fig.write_html(datadir+"\\Hist_"+name+".html")
+    #fig.update_layout(title = "Historic "+name+" stock prices", yaxis_title = "USD", xaxis_title = "Period "+Histperiod, 
+     #                 xaxis = dict(tickmode = "array", tickvals = data.reset_index().index.tolist(), ticktext = textdate))
+    fig.write_html(datadir+"\\Hist_"+name+".html")
     
 
 

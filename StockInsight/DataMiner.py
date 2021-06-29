ca6e657147b0f4f84a07f45aaf4fbabce19e615a
Samuel Rod-Wal <83095093+SRodWal@users.noreply.github.com>
2021-06-29 15:14:48 -0500

    update

--- StockInsight/DataMiner.py
+++ StockInsight/DataMiner.py
@@ -23,7 +23,7 @@ StockList = ["BA","FSLR","PCRFY"] #List of Stocks
 NameList = ["Boeing Co", "First Solar, Inc", "Panasonic Corp"] # Names of the tickers
 period = "5wk" #Time backwards
 interval = "5m" # Time between mesurements  
-filedir = os.getcwd()
+
 #Export lists of stocks
 #tickerdf = pd.DataFrame()
 #tickerdf["Ticker"] = StockList
@@ -31,22 +31,24 @@ filedir = os.getcwd()
 #tickerdf.to_excel("TickersList.xlsx", index = False)
 
 ###Objective folder
-datadir = filedir+"\\DATA"  
+
 ##Writes data in excels
 #create excels for each ticker: Historic (daily), Last 6 months (5min intervals)
-for ticker,name in zip(StockList, NameList):
-    Rawdata = yf.Ticker(ticker) #Retrieves all stock information
-    data = Rawdata.history(period = "max") # Stock daily historic data
-    textdate = [time.strftime("%m/%d/%Y") for time in data.index]
-    Histperiod = textdate[0]+" - "+textdate[-1]
-    candledata = go.Candlestick(x = data.index.tolist(), open = data.Open, high = data.High,low = data.Low, close = data.Close, name = "Pricing")
-    volumedata = go.Scatter(x = data.index.tolist(), y = data.Volume, name = "Volume", yaxis = "y2")
-    fig = make_subplots(specs = [[{"secondary_y": True}]])
-    fig.add_trace(candledata)
-    fig.add_trace(volumedata, secondary_y = True)
-    fig.update_layout(title = "Historic "+name+" stock prices", yaxis_title = "USD", xaxis_title = "Period "+Histperiod, 
-                      )
-    fig.write_html(datadir+"\\Hist_"+name+".html")
+def Historic_TickerGraph( StockList, Namelist ):
+    for ticker,name in zip(StockList, NameList):
+        filedir = os.getcwd()
+        datadir = filedir+"\\DATA"  
+        Rawdata = yf.Ticker(ticker) #Retrieves all stock information
+        data = Rawdata.history(period = "max") # Stock daily historic data
+        textdate = [time.strftime("%m/%d/%Y") for time in data.index]
+        Histperiod = textdate[0]+" - "+textdate[-1]
+        candledata = go.Candlestick(x = data.index.tolist(), open = data.Open, high = data.High,low = data.Low, close = data.Close, name = "Pricing")
+        volumedata = go.Scatter(x = data.index.tolist(), y = data.Volume, name = "Volume", yaxis = "y2")
+        fig = make_subplots(specs = [[{"secondary_y": True}]])
+        fig.add_trace(candledata)
+        fig.add_trace(volumedata, secondary_y = True)
+        fig.update_layout(title = "Historical "+name+" stock prices", yaxis_title = "USD", xaxis_title = "Period "+Histperiod,                       )
+        fig.write_html(datadir+"\\Hist_"+name+".html")
     
 
 

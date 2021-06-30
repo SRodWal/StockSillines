d71d347496e2d121f6c43a93d906256040aff9ee
Samuel Rod-Wal <83095093+SRodWal@users.noreply.github.com>
2021-06-29 20:32:50 -0500

    BMV

--- StockInsight/DataMiner.py
+++ StockInsight/DataMiner.py
@@ -19,7 +19,7 @@ import plotly.express as px
 
 
 ###Define input variables
-StockList = ["BA","FSLR","PCRFY"] #List of Stocks
+StockList = ["BA.MX","FSLR.MX","0jn9n.MX"] #List of Stocks
 NameList = ["Boeing Co", "First Solar, Inc", "Panasonic Corp"] # Names of the tickers
 period = "5wk" #Time backwards
 interval = "5m" # Time between mesurements  
@@ -34,23 +34,33 @@ interval = "5m" # Time between mesurements
 
 ##Writes data in excels
 #create excels for each ticker: Historic (daily), Last 6 months (5min intervals)
-def Historic_TickerGraph( StockList, Namelist ):
-    for ticker,name in zip(StockList, NameList):
+def Historical_TickerGraph( StockList ):
+    for ticker in StockList:
         filedir = os.getcwd()
         datadir = filedir+"\\DATA"  
         Rawdata = yf.Ticker(ticker) #Retrieves all stock information
+        datainf = Rawdata.info
+        tickername = datainf["longName"]
+        if "/" in tickername:
+            tickername = tickername.replace("/","")
+        if "." in tickername:    
+            tickername = tickername.replace(".","")
+        currency = datainf["currency"]
         data = Rawdata.history(period = "max") # Stock daily historic data
         textdate = [time.strftime("%m/%d/%Y") for time in data.index]
         Histperiod = textdate[0]+" - "+textdate[-1]
-        candledata = go.Candlestick(x = data.index.tolist(), open = data.Open, high = data.High,low = data.Low, close = data.Close, name = "Pricing")
-        volumedata = go.Scatter(x = data.index.tolist(), y = data.Volume, name = "Volume", yaxis = "y2")
+        x = data.index.tolist()
+        candledata = go.Candlestick(x = x, open = data.Open, high = data.High,low = data.Low, close = data.Close, name = "Pricing")
+        volumedata = go.Scatter(x = x, y = data.Volume, name = "Volume", yaxis = "y2")
+        divdata = go.Scatter(x=x, y= data.Dividends, name = "Dividends", yaxis = "y3")
         fig = make_subplots(specs = [[{"secondary_y": True}]])
         fig.add_trace(candledata)
         fig.add_trace(volumedata, secondary_y = True)
-        fig.update_layout(title = "Historical "+name+" stock prices", yaxis_title = "USD", xaxis_title = "Period "+Histperiod,                       )
-        fig.write_html(datadir+"\\Hist_"+name+".html")
+        fig.add_trace(divdata, secondary_y = True)
+        fig.update_layout(title = "Historical Data: "+tickername+"  ---   "+" Ticker:"+ticker.upper(), yaxis_title = currency, xaxis_title = "Period "+Histperiod,                       )
+        fig.write_html(datadir+"\\Hist_"+tickername+".html")
     
-
+Historical_TickerGraph(StockList)
 
 
 

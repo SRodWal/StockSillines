91249815c690ab2dd74e4642c7b92b923e7ef006
Samuel Rod-Wal <83095093+SRodWal@users.noreply.github.com>
2024-11-28 23:07:49 -0600

    WK48

--- Portfolio Metrics/markowitz_portfolio_optimizer.py
+++ Portfolio Metrics/markowitz_portfolio_optimizer.py
@@ -39,7 +39,7 @@ def Selector(lst):
     return selected_items, risk_aversion
 
 # Create the dataframe from the given data
-f_dir = r"D:/Professional_WorkTools/Github/StockSillines/Open Positions.xlsx"
+f_dir = r"D:/Professional_WorkTools/Github/StockSillines/Portfolio Expected Returns.xlsx"
 portfolio_df = pd.read_excel(f_dir)
 portfolio_df = portfolio_df[["Chapter", "Ticker", "Volume", "Price", "Expected Return QoQ%", "Volatility", "Value"]]
 Selected_Chapters, risk_aversion = Selector(list(set(portfolio_df.Chapter)))

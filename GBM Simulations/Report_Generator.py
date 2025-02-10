0f9fac8b2348eba64d505dd341eec2fa3b679226
Samuel Rod-Wal <83095093+SRodWal@users.noreply.github.com>
2025-02-09 20:56:28 -0600

    WK06F Update

--- GBM Simulations/Report_Generator.py
+++ GBM Simulations/Report_Generator.py
@@ -202,4 +202,4 @@ if __name__ == "__main__":
         kpi_df = pd.DataFrame(kpis)
 
         # Generate HTML report
-        guardar_graficas_html("GBMS_Report_120D.html", kpi_df, tickers_info, figs_dict)
+        guardar_graficas_html("GBMS_Report_10Y.html", kpi_df, tickers_info, figs_dict)
